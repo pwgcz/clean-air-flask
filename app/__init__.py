22cb7b33364eb230d4a6e78b261dcd4949c4277b
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-13 21:19:13 +0200

    Heroku deploy

--- app/__init__.py
+++ app/__init__.py
@@ -14,7 +14,7 @@ ma = Marshmallow()
 def create_app():
     app = Flask(__name__)
 
-    app.config.from_object(DevelopmentConfig)
+    app.config.from_object(ProductionConfig)
 
     cr.init_app(app)
     db.init_app(app)
