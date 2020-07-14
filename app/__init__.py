1ae11f4fa91850e80c1246a5f7d274164bec74a3
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-14 16:12:57 +0200

    inedx.html route

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ ma = Marshmallow()
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='./build', static_url_path='/')
 
     app.config.from_object(ProductionConfig)
 
--- app/send_static/__init__.py
+++ app/send_static/__init__.py
@@ -1,8 +1,6 @@
-import os
-
 from flask import Blueprint
 
 
-send_static = Blueprint("send_static", __name__, static_folder='./build', static_url_path='/static')
+send_static = Blueprint("send_static", __name__)
 
 from . import url
