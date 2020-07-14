d06cfc198eb2d0002826d42e3749cfcfa990ed0b
Pawel Grabczak <grabczak.pa@gmail.com>
2020-07-14 15:47:38 +0200

    inedx.html route

--- app/send_static/__init__.py
+++ app/send_static/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Blueprint
 
-static_folder = os.path.join(os.curdir, 'build')
-send_static = Blueprint("send_static", __name__, static_folder=static_folder, static_url_path='/')
-print(static_folder)
+
+send_static = Blueprint("send_static", __name__, static_folder='./build', static_url_path='/static')
+
 from . import url
